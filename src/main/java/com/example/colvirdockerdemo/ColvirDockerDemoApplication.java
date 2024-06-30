e9edc5176a09977938afbcac3f64930a582e2d45
makkov <maxim_86nva@mail.ru>
2024-06-30 17:35:01 +0300

    init project

---
+++ src/main/java/com/example/colvirdockerdemo/ColvirDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.colvirdockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColvirDockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColvirDockerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/colvirdockerdemo/ColvirDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colvirdockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColvirDockerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
